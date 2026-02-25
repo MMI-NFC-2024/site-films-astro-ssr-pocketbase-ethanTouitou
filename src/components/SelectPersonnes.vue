79ca223134ff59e3a8582df0d30780e87f51e2e3
ethanTouitou <150148202+ethanTouitou@users.noreply.github.com>
2026-02-25 09:00:51 +0100

    composant vue

---
+++ src/components/SelectPersonnes.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Multiselect from '@vueform/multiselect';
+import type { PersonneResponse } from '../pocketbase-types';
+import { ref } from 'vue';
+const props = defineProps<{
+    personnes: PersonneResponse[];
+}>();
+const personneId = ref()
+</script>
+
+<template>
+      <Multiselect
+                v-model="personneId"
+                :options="personnes"
+                label="nom"
+                value-prop="id"
+              />
+
+</template>
+
+<style src="@vueform/multiselect/themes/default.css"></style>
